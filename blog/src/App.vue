708bffb6e51891b9fed5be6ad3dce33bf2530db0
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-27 20:27:56 -0300

    - Adicionando as primeiras informações no app - Criando o componente CardDefault - Adicionando a lib TailWind CSS - Adicionando a lib Oh-Vue-Icons - Personalizando a página inicial

---
+++ blog/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import CardDefault from '@/components/Cards/CardDefault.vue'
+</script>
+
+<template>
+  <div class="container mx-auto">
+    Pagina Home
+
+    <CardDefault title="teste" />
+  </div>
+</template>
+
+<style lang="css" scoped></style>
---
+++ blog/src/components/Cards/CardDefault.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <div class="w-80 flex justify-center">
+    <div
+      class="w-full h-80 flex justify-center items-stretch rounded-lg border border-red-700 shadow-lg"
+    >
+      <div class=""></div>
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
