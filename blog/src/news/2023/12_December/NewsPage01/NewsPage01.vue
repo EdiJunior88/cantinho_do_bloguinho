a24dcb7f38b93c3342cd635424b20fa3ee88a714
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-05 20:38:53 -0300

    - Adicionando novas imagens - Refatorando o código do componente NewsPage01.vue

--- blog/src/news/2023/12_December/NewsPage01/NewsPage01.vue
+++ blog/src/news/2023/12_December/NewsPage01/NewsPage01.vue
@@ -25,7 +25,7 @@ import { cardTitle_12_01, cardDate_12_01 } from '@/components/Titles/2023/12_Dec
 
       <p class="py-2">
         Vamos utilizar como um exemplo a seguinte situação: Eu quero que o
-        <span class="marcatexto">texto fique somente com a fonte maior</span> a partir do
+        <span class="marcatexto">texto fique somente com a fonte maior</span> contanto que o
         <span class="marcatexto">tamanho da tela seja maior ou igual a 1200 pixels</span>, nesse
         caso irei fazer da seguinte maneira:
       </p>
@@ -33,10 +33,69 @@ import { cardTitle_12_01, cardDate_12_01 } from '@/components/Titles/2023/12_Dec
       <img src="./assets/code01.webp" alt="exemplo código" />
 
       <p class="py-2">
-        Olhando de primeira fica meio difícil interpretar o que diz na parte do código
-        <code class="codigo">@media (min-width: 1200px)</code>, demora um pouco e você percebe que
-        ele diz que a largura da tela seja maior ou igual a 1200 pixels a fonte do texto será de
-        <code class="codigo">3rem</code>.
+        Olhando pela primeira vez fica meio difícil interpretar o que diz na parte do código
+        <code class="codigo">@media (min-width: 1200px)</code>, vai demorar um pouquinho mas você
+        percebe que o código diz que
+        <strong>caso a largura da tela seja maior ou igual a 1200 pixels</strong> a fonte do texto
+        será de <code class="codigo">3rem</code>.
+      </p>
+
+      <p class="py-2">
+        Agora foi introduzido uma nova sintaxe utilizando
+        <span class="marcatexto">operadores de comparação</span> do tipo
+        <code class="codigo">&lt;, &gt;, &lt;=, &gt;=, =</code>
+      </p>
+
+      <p class="py-2">
+        📌 <code class="codigo">&lt;</code> avalia se um valor é
+        <span class="marcatexto">menor que</span> outro valor
+        <br />
+        📌 <code class="codigo">&gt;</code> avalia se um valor é
+        <span class="marcatexto">maior que</span> outro valor
+        <br />
+        📌 <code class="codigo">=</code> avalia se um valor é
+        <span class="marcatexto">igual</span> que outro valor
+        <br />
+        📌 <code class="codigo">&lt;=</code> avalia se um valor é
+        <span class="marcatexto">menor ou igual</span> a outro valor
+        <br />
+        📌 <code class="codigo">&gt;=</code> avalia se um valor é
+        <span class="marcatexto">maior ou igual</span> a outro valor
+        <br />
+      </p>
+
+      <p class="py-2">Vejamos em um exemplo para que você fixe melhor esse assunto:</p>
+
+      <img src="./assets/code02.webp" alt="exemplo código" />
+
+      <p class="py-2">
+        Agora ficou mais fácil e intuitivo verificar em qual o ponto da responsividade ficará a
+        largura. Inclusive esse tipo de sintaxe funciona também em
+        <span class="marcatexto">comparação de intervalos</span>, vejamos outro exemplo:
+      </p>
+
+      <img src="./assets/code03.webp" alt="exemplo código" />
+
+      <p class="py-2">
+        Explicando brevemente o <span class="marcatexto">segundo código</span>:
+        <strong>
+          O estilo é aplicado aos elementos quando a largura da janela de visualização está entre
+          400 pixels e 1000 pixels.
+        </strong>
+      </p>
+
+      <p class="py-2">
+        Caso você esteja curioso em implementar essa nova sintaxe em seus códigos CSS, não esqueça de
+        verificar no site <span class="marcatexto">Can In Use</span> através desse link:
+        <code class="codigo">
+          <a
+            href="https://caniuse.com/?search=CSS%20Media%20Query%20Range%20Syntax"
+            target="_blank"
+            rel="noopener noreferrer"
+            >https://caniuse.com/</a
+          >
+        </code>
+        <img src="./assets/code04.webp" alt="exemplo página" />
       </p>
     </section>
   </div>
