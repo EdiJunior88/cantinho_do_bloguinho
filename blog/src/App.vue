c0043b0df5d3d564c88750b6957bbe37d2e7273e
EdiJunior88 <er.moura88@yahoo.com.br>
2024-02-06 17:55:35 -0300

    - Adicionando o Vercel Analytics no projeto Visualizações do site em tempo real através da Vercel

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -2,9 +2,12 @@
 /* Componente mãe que engloba todos os componentes o projeto */
 import { RouterView } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+/* Componente de análise de views do site pela Vercel */
+import { Analytics } from '@vercel/analytics/react'
 </script>
 
 <template>
   <HomeView />
   <RouterView />
+  <Analytics />
 </template>
