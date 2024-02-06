28117380b45561009362fb85b334d7fc6eeccc4d
EdiJunior88 <er.moura88@yahoo.com.br>
2024-02-06 18:05:55 -0300

    - Corrigindo o web analytics da Vercel no projeto

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -2,12 +2,15 @@
 /* Componente mãe que engloba todos os componentes o projeto */
 import { RouterView } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-/* Componente de análise de views do site pela Vercel */
-import { Analytics } from '@vercel/analytics/react'
+
+/* Componente de analise de views do site pela Vercel */
+import { inject } from '@vercel/analytics';
+
+/* Chamando o Componente de analise de views */
+inject();
 </script>
 
 <template>
   <HomeView />
   <RouterView />
-  <Analytics />
 </template>
