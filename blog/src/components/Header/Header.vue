dd278ea69af784657bd2747ebf9753f00afaf57c
EdiJunior88 <er.moura88@yahoo.com.br>
2024-02-28 14:22:48 -0300

    - Estilizando o CSS dos componentes Header.astro Header.vue Layout.astro index.astro

--- blog/src/components/Header/Header.vue
+++ blog/src/components/Header/Header.vue
@@ -1,10 +1,38 @@
 <script setup lang="ts">
-defineProps(["title"]);
+defineProps(["title", "home", "about"]);
 </script>
 
 <template>
-  <div>
-    <slot />
-    <h1>{{ title }}</h1>
+  <div
+    class="mx-auto flex h-20 max-w-screen-md items-center justify-between bg-[#a0db44] px-8"
+  >
+    <div class="flex items-center justify-center gap-3">
+      <slot />
+      <h1 class="font-header text-2xl">{{ title }}</h1>
+    </div>
+
+    <div class="font-roboto flex gap-7 text-lg font-bold">
+      <a href="/" target="_self"
+        ><span>{{ home }}</span></a
+      >
+      <span>{{ about }}</span>
+    </div>
   </div>
 </template>
+
+<style lang="css" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+.font-header {
+  font-family: "Bungee Spice", sans-serif;
+  background: #00870e;
+  background: linear-gradient(to top, #00870e 0%, #0a0094 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.font-roboto {
+  font-family: "Roboto", sans-serif;
+}
+</style>
