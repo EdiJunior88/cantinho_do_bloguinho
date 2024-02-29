09f239533f006446e59ddb628c203fc4d270b2c4
EdiJunior88 <er.moura88@yahoo.com.br>
2024-02-28 21:07:49 -0300

    - Retirando as props inutilizadas: Header.vue

--- blog/src/components/Header/Header.vue
+++ blog/src/components/Header/Header.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
-defineProps({
-  title: {
-    type: String,
-    required: true,
-  },
-  home: {
-    type: String,
-    required: true,
-  },
-  about: {
-    type: String,
-    required: true,
-  },
-});
+  defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+  });
 </script>
 
 <template>
@@ -23,13 +15,13 @@ defineProps({
 </template>
 
 <style lang="css" scoped>
-@import url("https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap");
 
-.font-header {
-  font-family: "Bungee Spice", sans-serif;
-  background: #00870e;
-  background: linear-gradient(to top, #00aa11 0%, #0a0094 100%);
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
+  .font-header {
+    font-family: "Bungee Spice", sans-serif;
+    background: #00870e;
+    background: linear-gradient(to top, #00aa11 0%, #0a0094 100%);
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
 </style>
